642031962d62587c3bdaeb5b7e805866ee7494ea
David Freitag <freitagdavid@outlook.com>
2019-04-16 11:53:18 -0400

    projects/graph/src/graph: Implemented recursive dft

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -36,28 +36,48 @@ class Graph:
                     queue.append(i)
         print(visited)
 
-    def dft(self, start):
-        stack = [start]
-        visited = set()
-        while len(stack) > 0:
-            vertex = stack.pop()
-            if vertex not in visited:
-                visited.add(vertex)
-                for i in self.vertices[vertex]:
-                    stack.append(i)
-        print(visited)
+    def dft(self, v, visited=set(), output=set()):
+        output.add(v)
+        for i in self.vertices[v]:
+            if i not in visited:
+                visited.add(v)
+                self.dft(i, visited, output)
+        return output
+
+    # def dft_util(self, v, visited):
+    #     visited[int(v)] = True
+    #     print(v)
+    #     for i in self.vertices[v]:
+    #         if not visited[int(i)]:
+    #             self.dft_util(i, visited)
+
+    # def dft(self, v):
+    #     visited = [False]*(len(self.vertices))
+    #     return self.dft_util(v, visited)
 
 
 graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.bft('0')
-graph.dft('0')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+graph.add_directed_edge('5', '3')
+graph.add_directed_edge('6', '3')
+graph.add_directed_edge('7', '1')
+graph.add_directed_edge('4', '7')
+graph.add_directed_edge('1', '2')
+graph.add_directed_edge('7', '6')
+graph.add_directed_edge('2', '4')
+graph.add_directed_edge('3', '5')
+graph.add_directed_edge('2', '3')
+graph.add_directed_edge('4', '6')
+graph.bft('1')
+print(graph.dft('1'))
+# print(graph.dfs('1'))
 
-pp = pprint.PrettyPrinter(indent=4)
+# pp = pprint.PrettyPrinter(indent=4)
 
-pp.pprint(graph.vertices)
+# pp.pprint(graph.vertices)
